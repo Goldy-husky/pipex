6d80ade561d341442fe8d2c7caf6c3b3264e948c
Christophe Mehay <cmehay@student.42.fr>
2013-12-30 18:20:56 +0100

    corrections

--- srcs/files.c
+++ srcs/files.c
@@ -6,7 +6,7 @@
 /*   By: cmehay <cmehay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/30 15:01:43 by cmehay            #+#    #+#             */
-/*   Updated: 2013/12/30 17:27:38 by cmehay           ###   ########.fr       */
+/*   Updated: 2013/12/30 18:18:00 by cmehay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_fd	*open_files(char ***param)
 	if (!(fd = (t_fd*) malloc(sizeof(t_fd) * 2)))
 		return (NULL);
 	if ((fd[0] = open(param[0][0], O_RDONLY)) == -1
-		|| (fd[1] = open(param[0][0], O_WRONLY)) == -1)
+		|| (fd[1] = open(param[3][0], O_WRONLY | O_CREAT, 0666)) == -1)
 		return (NULL);
 	return (fd);
 }
